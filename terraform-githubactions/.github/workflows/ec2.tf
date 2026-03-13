5815dfbe27a84abda92ce46b62ca2739c56abfce
Satya <vvsatyanarayana.swarnapudi@gmail.com>
2026-03-13 17:44:46 +0530

    Delete terraform-githubactions directory
    i want to delete this as i have mis pushed my files into this repo

--- terraform-githubactions/.github/workflows/ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "github_ec2" {
-  ami           = "ami-0c7217cdde317cfec"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "github-actions-ec2"
-    Environment = "Dev"
-  }
-}
